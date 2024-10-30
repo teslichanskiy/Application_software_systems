0089f644c2db21fb25208b68f1bf38f626f63d66
teslichanskiy <tesla.kyrgyz2022@gmail.com>
2024-10-30 22:57:39 +0300

    Add files via upload

---
+++ lab13(drawing_app)/ClassLibrary4/Class1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary4
+{
+    public abstract class Shape
+    {
+        public Color Color { get; set; }
+        public Pen Pen { get; private set; }
+
+        public Shape(Color color)
+        {
+            Color = color;
+            Pen = new Pen(color, 2); // Толщина линии по умолчанию - 2 пикселя
+        }
+
+        public abstract void Draw(Graphics g);
+    }
+
+    public class Line : Shape
+    {
+        public Point Start { get; set; }
+        public Point End { get; set; }
+
+        public Line(Color color, Point start, Point end) : base(color)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            g.DrawLine(Pen, Start, End);
+        }
+    }
+
+    public class Curve : Shape
+    {
+        public List<Point> Points { get; set; }
+
+        public Curve(Color color, List<Point> points) : base(color)
+        {
+            Points = points;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            if (Points.Count > 1)
+            {
+                g.DrawCurve(Pen, Points.ToArray());
+            }
+        }
+    }
+
+    public class AreaFill : Shape
+    {
+        public List<Point> Points { get; set; }
+
+        public AreaFill(Color color, List<Point> points) : base(color)
+        {
+            Points = points;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            if (Points.Count > 2)
+            {
+                g.FillPolygon(new SolidBrush(Color), Points.ToArray());
+            }
+        }
+    }
+
+    public class BezierCurve : Shape
+    {
+        public Point Start { get; set; }
+        public Point ControlPoint1 { get; set; }
+        public Point ControlPoint2 { get; set; }
+        public Point End { get; set; }
+
+        public BezierCurve(Color color, Point start, Point controlPoint1, Point controlPoint2, Point end) : base(color)
+        {
+            Start = start;
+            ControlPoint1 = controlPoint1;
+            ControlPoint2 = controlPoint2;
+            End = end;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            g.DrawBezier(Pen, Start, ControlPoint1, ControlPoint2, End);
+        }
+    }
+
+    public class ShapeCollection
+    {
+        public List<Shape> Shapes { get; set; } = new List<Shape>();
+
+        public void AddShape(Shape shape)
+        {
+            Shapes.Add(shape);
+        }
+
+        public void DrawShapes(Graphics g)
+        {
+            foreach (var shape in Shapes)
+            {
+                shape.Draw(g);
+            }
+        }
+    }
+}
---
+++ lab8(chain_of_responsibility)/lab8/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+
+namespace lab8
+{
+    public partial class MainForm : Form
+    {
+        private KeyHandlerChain chain;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            chain = new KeyHandlerChain();
+            chain.AddHandler(new LetterHandler());
+            chain.AddHandler(new DigitHandler());
+            chain.AddHandler(new DefaultHandler());
+
+            textBox1.KeyPress += TextBox1_KeyPress;
+
+            Button finishButton = new Button { Text = "Завершить ввод", Location = new System.Drawing.Point(170, 100), Width = 150 };
+            finishButton.Click += FinishInputButton_Click;
+            this.Controls.Add(finishButton);
+        }
+
+        private void TextBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Передаем нажатую клавишу в цепочку обработчиков
+            chain.Handle(e.KeyChar);
+        }
+
+        private void FinishInputButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ввод завершен.");
+            Thread.Sleep(500);
+
+            // Завершение программы
+            Application.Exit();
+        }
+    }
+
+
+    // Базовый класс обработчика
+    abstract class KeyHandler
+    {
+        public KeyHandler nextHandler;
+
+        public void SetNext(KeyHandler next)
+        {
+            nextHandler = next;
+        }
+
+        public abstract void Handle(char keyChar);
+    }
+
+    // Обработчик для букв
+    class LetterHandler : KeyHandler
+    {
+        public override void Handle(char keyChar)
+        {
+            if (char.IsLetter(keyChar))
+            {
+                MessageBox.Show($"Обработка буквы: {keyChar}");
+            }
+            else if (nextHandler != null)
+            {
+                nextHandler.Handle(keyChar);
+            }
+        }
+    }
+
+    // Обработчик для цифр
+    class DigitHandler : KeyHandler
+    {
+        public override void Handle(char keyChar)
+        {
+            if (char.IsDigit(keyChar))
+            {
+                MessageBox.Show($"Обработка цифры: {keyChar}");
+            }
+            else if (nextHandler != null)
+            {
+                nextHandler.Handle(keyChar);
+            }
+        }
+    }
+
+    // Обработчик по умолчанию
+    class DefaultHandler : KeyHandler
+    {
+        public override void Handle(char keyChar)
+        {
+            MessageBox.Show($"Неизвестный символ: {keyChar}");
+        }
+    }
+
+    // Класс цепочки обработчиков
+    class KeyHandlerChain
+    {
+        private KeyHandler firstHandler;
+
+        public void AddHandler(KeyHandler handler)
+        {
+            if (firstHandler == null)
+            {
+                firstHandler = handler;
+            }
+            else
+            {
+                KeyHandler currentHandler = firstHandler;
+                while (currentHandler.nextHandler != null)
+                {
+                    currentHandler = currentHandler.nextHandler;
+                }
+                currentHandler.SetNext(handler);
+            }
+        }
+
+        public void Handle(char keyChar)
+        {
+            if (firstHandler != null)
+            {
+                firstHandler.Handle(keyChar);
+            }
+        }
+    }
+}
